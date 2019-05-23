eb62b6331b5aa0c41d42648ca6fd347b7ccb09c6
Nina Kazzim <kazzim@icloud.com>
2019-05-23 14:43:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  counter = 10
+   while counter < num
+    puts "#{num} SECOND(S)!"
+    counter -= 1
+  end
+   puts "HAPPY NEW YEAR!"
+  
 end
