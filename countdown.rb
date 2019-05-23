bf8b565e0c2c7c5b049c8c6ef2ec2993cf797f5b
Nina Kazzim <kazzim@icloud.com>
2019-05-23 17:16:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(counter)
   end
    return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(counter)
+   while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+   return "HAPPY NEW YEAR!"
+end
